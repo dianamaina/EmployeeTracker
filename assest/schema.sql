99be2ea76fa7429a9fe01824d2356bf099906d90
diana maina <dianamainachui@gmail.com>
2020-11-12 08:46:56 -0500

    modified schema and server

--- assest/schema.sql
+++ assest/schema.sql
@@ -1,31 +1,28 @@
   
-DROP DATABASE IF EXISTS employee_tracker_db;
-CREATE DATABASE employee_tracker_db;
+DROP DATABASE IF EXISTS empmanagement_db;
+CREATE DATABASE empmanagement_db;
 
-USE employee_tracker_db;
+USE empmanagement_db;
 
-CREATE TABLE department (
-department_id INTEGER(3) NOT NULL PRIMARY KEY,
-department_name VARCHAR(30) NOT NULL 
+CREATE TABLE department(
+    id INT NOT NULL,
+    name VARCHAR(30),
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE role (
-role_id INTEGER(3) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-title VARCHAR(50) NOT NULL, 
-salary DECIMAL(10,2), 
-department_id INTEGER(3), 
-CONSTRAINT fk_department_id FOREIGN KEY (department_id) REFERENCES department(department_id)
-ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE role(
+    id INT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE employee (
-id INTEGER(3) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-first_name VARCHAR(30) NOT NULL,
-last_name VARCHAR(30) NOT NULL,
-role_id INTEGER(3), 
-CONSTRAINT fk_role_id FOREIGN KEY (role_id) REFERENCES role(role_id)
-ON DELETE CASCADE ON UPDATE CASCADE,
-manager_id INTEGER(3) DEFAULT null,
-CONSTRAINT fk_manager_id FOREIGN KEY (manager_id) REFERENCES employee(id)
-ON DELETE CASCADE ON UPDATE CASCADE
-);
+CREATE TABLE employee(
+    id INT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+  PRIMARY KEY (id)
+)
--- assest/server.js
+++ assest/server.js
@@ -1,235 +1,225 @@
+
   
-// DEPENDENCIES
-const mysql = require("mysql");
 const inquirer = require("inquirer");
-const cTable = require("console.table");
+const mysql = require("mysql");
 
-// Creates a Connection with the Database
 const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Chui@gt2020",
-  database: "employee_tracker_db",
+    host: "localhost",
+    // Your port; if not 3306
+    port: 3306,
+    // Your username
+    user: "root",
+    // Your password
+    password: "Chui@gt2020",
+    database: "employees_db"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  runSearch();
+connection.connect(function (err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    console.log("connection has begun");
+    start();
 });
 
-function runSearch() {
-  inquirer
-    .prompt({
-      name: "action",
-      type: "rawlist",
-      message: "What would you like to do?",
-      choices: [
-        "Find employee by Id",
-        "Find all employees who appear more than once",
-        "Find data within a specific range",
-        "Search for a specific role",
-        "Find employees with a top salary"
-      ]
+function start() {
+    inquirer
+        .prompt({
+            name: "selection",
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["View all Departments", "View all Roles", "View all Employees", "Add a Department", "Add a Role", "Add an Employee", "Update Role", "Exit"]
+        }).then(({ selection }) => {      // used destructuring to access key
+            // based on their answer, either call the bid or the post functions
+            //use a switch case to determine which function executes
+            switch (selection) {
+                case "View all Departments":
+                    return viewDepartment();
+                    break;
+                case "View all Roles":
+                    return viewRoles();
+                    break;
+                case "View all Employees":
+                    return viewEmployees();
+                    break;
+                case "Add a Department":
+                    return addDepartment();
+                    break;
+                case "Add a Role":
+                    return addRole();
+                    break;
+                case "Add an Employee":
+                    return addEmployee();
+                    break;
+                case "Update Role":
+                    return updateRole();
+                    break;
+                default:
+                    console.log("Goodbye");
+                    connection.end();
+            }
+        })
+}
+
+function viewDepartment() {
+    const query = "SELECT * FROM department";
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        start();
     })
-    .then(function(answer) {
-      switch (answer.action) {
-      case "Find employee by role":
-       employeeSearch();
-        break;
-
-      case "Find all employees who appear more than once":
-        multiSearch();
-        break;
-
-      case "Find data within a specific range":
-        rangeSearch();
-        break;
-
-      case "Search for a specific employee":
-      roleSearch();
-        break;
-
-      case "Find employees with a top salary":
-      roleAndAlbumSearch();
-        break;
-      }
-    });
 }
 
-function employeeSearch() {
-  inquirer
-    .prompt({
-      name: "employee",
-      type: "input",
-      message: "What employee would you like to search for?"
+function viewRoles() {
+    const query = "SELECT * FROM role";
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        start();
     })
-    .then(function(answer) {
-      var query = "SELECT position, role, year FROM top5000 WHERE ?";
-      connection.query(query, { employee: answer employee }, function(err, res) {
-        for (var i = 0; i < res.length; i++) {
-          console.log("Position: " + res[i].position + " ||role: " + res[i]role + " || Year: " + res[i].year);
-        }
-        runSearch();
-      });
-    });
 }
 
-function multiSearch() {
-  var query = "SELECT employee FROM top5000 GROUP BY employee HAVING count(*) > 1";
-  connection.query(query, function(err, res) {
-    for (var i = 0; i < res.length; i++) {
-      console.log(res[i] empolyee);
-    }
-    runSearch();
-  });
-}
-
-function rangeSearch() {
-  inquirer
-    .prompt([
-      {
-        name: "start",
-        type: "input",
-        message: "Enter starting position: ",
-        validate: function(value) {
-          if (isNaN(value) === false) {
-            return true;
-          }
-          return false;
-        }
-      },
-      {
-        name: "end",
-        type: "input",
-        message: "Enter ending position: ",
-        validate: function(value) {
-          if (isNaN(value) === false) {
-            return true;
-          }
-          return false;
-        }
-      }
-    ])
-    .then(function(answer) {
-      var query = "SELECT positionrole employee,year FROM top5000 WHERE position BETWEEN ? AND ?";
-      connection.query(query, [answer.start, answer.end], function(err, res) {
-        for (var i = 0; i < res.length; i++) {
-          console.log(
-            "Position: " +
-              res[i].position +
-              " ||role: " +
-              res[i]role +
-              " || employee: " +
-              res[i] employee +
-              " || Year: " +
-              res[i].year
-          );
-        }
-        runSearch();
-      });
-    });
-}
-
-functionroleSearch() {
-  inquirer
-    .prompt({
-      name: role",
-      type: "input",
-      message: "Whatrole would you like to look for?"
+function viewEmployees() {
+    const query = "SELECT * FROM employee";
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        start();
     })
-    .then(function(answer) {
-      console.log(answerrole);
-      connection.query("SELECT * FROM top5000 WHERE ?", {role: answerrole }, function(err, res) {
-        console.log(
-          "Position: " +
-            res[0].position +
-            " ||role: " +
-            res[0]role +
-            " || employee: " +
-            res[0]employee +
-        );
-        runSearch();
-      });
-    });
 }
 
-functionroleAndAlbumSearch() {
-  inquirer
-    .prompt({
-      name:  "employee",
-      type: "input",
-      message: "What employee would you like to search for?"
-    })
-    .then(function(answer) {
-      var query = "SELECT top_salary.year, top_albums.album, top_albums.position, top5000role, top5000 employee ";
-      query += "FROM top_albums INNER JOIN top5000 ON (top_albums employee = top5000 employee AND top_albums.year ";
-      query += "= top5000.year) WHERE (top_albums employee = ? AND top5000 employee = ?) ORDER BY top_albums.year, top_albums.position";
-
-      connection.query(query, [answer employee, answer employee], function(err, res) {
-        console.log(res.length + " matches found!");
-        for (var i = 0; i < res.length; i++) {
-          console.log(
-            i+1 + ".) " +
-              "Year: " +
-              res[i].year +
-              " Album Position: " +
-              res[i].position +
-              " || employee: " +
-              res[i] employee +
-              " ||role: " +
-              res[i]role +
-              " || Album: " +
-              res[i].album
-          );
+function addDepartment() {
+    //use inquirer to ask the user what department they would like to add
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "department",
+            message: "Enter the department's name:",
+            // validate: validateString
         }
-        runSearch();
-      });
+        //in the promise of inquirer add the department to the DB and the view all departments
+    ]).then(function (data) {
+        var newDepartment = data.department;
+        var query = "INSERT INTO department(name) VALUE (?)";
+
+        connection.query(query, newDepartment, function (err) {
+            if (err) throw err;
+
+            console.log("Your new department has been added.");
+            viewDepartment();
+        });
     });
 }
 
+function addRole() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "Enter role name?"
+        },
+        {
+            type: "input",
+            name: "salary",
+            message: "what's the salary? "
+        },
+        {
+            type: "input",
+            name: "departmentId",
+            message: "Whats the department id?"
+        }
+        //in the promise of inquirer add the department to the DB and the view all departments
+    ]).then(function (data) {
+        var newTitle = data.title;
+        var newSalary = data.salary;
+        var newDepartmentId = data.departmentId;
+        var query = "INSERT INTO role (title,salary,department_id) VALUE (?,?,?)";
 
-// Establishes the Connection with the Database
-connection.connect((err) => {
-  if (err) throw err;
+        connection.query(query, [newTitle, newSalary, newDepartmentId], function (err) {
+            if (err) throw err;
 
-  console.log("We track employees");
-
-  init();
-});init = () => {
-  inquirer
-    .prompt([
-      {
-        name: "userChoice",
-        type: "list",
-        message: "What would you like to do?",
-        choices: [
-          "View All Employees",
-          "View All Employees By Department",
-          "View All Employees By Manager",
-          "Add Employee",
-          "Remove Employee",
-          "Update Employee Role",
-          "Update Employee Manager",
-          "View All Roles",
-          "Add Role",
-          "Remove Role",
-          "View All Departments",
-          "Add Department",
-          "Remove Department",
-          "Exit",
-        ],
-        default: "View All Employees",
-      },
-    ])
-    .then((response) => {
-      filterFunctions(response.userChoice);
-    })
-    .catch((error) => {
-      console.log(error);
+            console.log("Your new roles have been added.");
+            viewRoles();
+        });
     });
-};
 
-// Swtich...case statements that complete the user's requested task
-filterFunctions = (userChoice) => {
-  let sql = ``;
+}
+
+function addEmployee() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "firstName",
+            message: "Enter first name?"
+        },
+        {
+            type: "input",
+            name: "lastName",
+            message: "Enter last name? "
+        },
+        {
+            type: "input",
+            name: "roleId",
+            message: "Whats the role id?"
+        },
+        {
+            type: "input",
+            name: "managerId",
+            message: "Whats the manager id?"
+        }
+
+        //in the promise of inquirer add the department to the DB and the view all departments
+    ]).then(function (data) {
+        var newFirstName = data.firstName;
+        var newLastName = data.lastName;
+        var newRoleId = data.roleId;
+        var newManagerId = data.managerId;
+        var query = "INSERT INTO employee (first_name,last_name,role_id,manager_id) VALUE (?,?,?,?)";
+
+        connection.query(query, [newFirstName, newLastName, newRoleId, newManagerId], function (err) {
+            if (err) throw err;
+
+            console.log("Your new roles have been added.");
+            viewEmployees();
+        });
+    });
+}
+
+function updateRole() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "newFirstName",
+            message: "Enter new first name?"
+        },
+        {
+            type: "input",
+            name: "newLastName",
+            message: "Enter new last name? "
+        },
+        {
+            type: "input",
+            name: "newRoleId",
+            message: "Whats the new role id?"
+        },
+        {
+            type: "input",
+            name: "newRoleId",
+            message: "Whats the new role id?"
+        }
+
+    ]).then(function (data) {
+        var newFirstName = data.firstName;
+        var newLastName = data.lastName;
+        var newRoleId = data.roleId;
+        var query = "UPDATE employee SET role_id = ? WHERE first_name = ? AND last_name = ?";
+
+        connection.query(query, [newRoleId, newFirstName, newLastName], function (err) {
+            if (err) throw err;
+
+            console.log("Your new roles have been added.");
+            viewEmployees();
+        });
+    });
+}
+
+// var query = "UPDATE employee SET role_id = ? WHERE first_name = ? AND last_name = ?";
