f5d659f35f6a1530db342d222b96026e23a49237
diana maina <dianamainachui@gmail.com>
2020-11-12 06:11:28 -0500

    modified schema

--- assest/schema.sql
+++ assest/schema.sql
@@ -6,14 +6,14 @@ USE employee_tracker_db;
 
 CREATE TABLE department (
 department_id INTEGER(3) NOT NULL PRIMARY KEY,
-department_name VARCHAR(30) NOT NULL -- Deparment Name
+department_name VARCHAR(30) NOT NULL 
 );
 
 CREATE TABLE role (
 role_id INTEGER(3) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-title VARCHAR(50) NOT NULL, -- Role Title
-salary DECIMAL(10,2), -- Role Salary
-department_id INTEGER(3), -- Holds reference to Department Role belongs to
+title VARCHAR(50) NOT NULL, 
+salary DECIMAL(10,2), 
+department_id INTEGER(3), 
 CONSTRAINT fk_department_id FOREIGN KEY (department_id) REFERENCES department(department_id)
 ON DELETE CASCADE ON UPDATE CASCADE
 );
@@ -22,10 +22,10 @@ CREATE TABLE employee (
 id INTEGER(3) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
-role_id INTEGER(3), -- Holds reference to Role Employee has
+role_id INTEGER(3), 
 CONSTRAINT fk_role_id FOREIGN KEY (role_id) REFERENCES role(role_id)
 ON DELETE CASCADE ON UPDATE CASCADE,
-manager_id INTEGER(3) DEFAULT null, -- Holds reference to another Employee that manages the Employee being created. May be null if this Employee has no Manager.
+manager_id INTEGER(3) DEFAULT null,
 CONSTRAINT fk_manager_id FOREIGN KEY (manager_id) REFERENCES employee(id)
 ON DELETE CASCADE ON UPDATE CASCADE
 );
--- assest/seeds.sql
+++ assest/seeds.sql
@@ -1,22 +1,22 @@
 INSERT INTO department (department_id, department_name) 
-VALUES (1, "Manager"), (2, "Sales"), (3, "Finance"), (4, "Office Management"), (5, "Human Resources"), (6, "Warehouse"), (7, "Information Technology");
+VALUES (1, "Manager"), (2, "Sales"), (3, "Finance"), (4, "Office Management"), (5, "Human Resources"), (6, "Warehouse"), 
 
 INSERT INTO role (title, salary, department_id) 
-VALUES ("Regional Manager", 60000, 1),
-("Salesman", 50000, 2),
-("Accountant", 40000, 3), 
-("Office Administrator", 40000, 4),
-("Customer Service Representative", 35000, 5),
-("Warehouse Foreman", 55000, 6);
+VALUES ("Regional Manager", 75000, 1),
+("Salesman", 40000, 2),
+("Accountant", 45000, 3), 
+("Office Administrator", 30000, 4),
+("Customer Service Representative", 20000, 7),
+("Warehouse Foreman", 25000, 6);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) 
-VALUES ("Michael", "Scott", 1, null),
-("Jim", "Halpert", 2, 1),
-("Dwight", "Schrute", 2, 1),
-("Kevin", "Malone", 3, 1),
-("Pam", "Beesley", 4, 1),
-("Kelly", "Kapor", 5, 1),
-("Darryl", "Philbin", 6, 1);
+VALUES ("Denis", "Hartman", 1, 1),
+("BIllie May", "Foster", 2, 2),
+("Noelle", "Johns", 2, 1),
+("Nathan", "Anderson", 3, 1),
+("Janet", "Foster", 4, 1),
+("Quevanze", "Harris", 5, 1),
+("Harry", "Brown", 6, 1);
 
 SELECT employee.id, employee.first_name, employee.last_name, role.title, department.department_name, role.salary, concat(manager.first_name , " " , manager.last_name) AS "manager"
 FROM employee
