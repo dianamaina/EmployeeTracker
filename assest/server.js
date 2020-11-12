6e671b808ee2929ac9e403ebab3fcf49d70a5fa8
diana maina <dianamainachui@gmail.com>
2020-11-12 06:25:40 -0500

    modified server.js

--- assest/server.js
+++ assest/server.js
@@ -1,18 +1,16 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+  
+// DEPENDENCIES
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
 
-var connection = mysql.createConnection({
+// Creates a Connection with the Database
+const connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "Chui@gt2020",
-  database: "employees_db"
+  database: "employee_tracker_db",
 });
 
 connection.connect(function(err) {
@@ -28,19 +26,19 @@ function runSearch() {
       message: "What would you like to do?",
       choices: [
         "Find employee by Id",
-        "Find all artists who appear more than once",
+        "Find all employees who appear more than once",
         "Find data within a specific range",
-        "Search for a specific song",
-        "Find artists with a top song and top album in the same year"
+        "Search for a specific role",
+        "Find employees with a top salary"
       ]
     })
     .then(function(answer) {
       switch (answer.action) {
-      case "Find songs by artist":
-        artistSearch();
+      case "Find employee by role":
+       employeeSearch();
         break;
 
-      case "Find all artists who appear more than once":
+      case "Find all employees who appear more than once":
         multiSearch();
         break;
 
@@ -48,29 +46,29 @@ function runSearch() {
         rangeSearch();
         break;
 
-      case "Search for a specific song":
-        songSearch();
+      case "Search for a specific employee":
+      roleSearch();
         break;
 
-      case "Find artists with a top song and top album in the same year":
-        songAndAlbumSearch();
+      case "Find employees with a top salary":
+      roleAndAlbumSearch();
         break;
       }
     });
 }
 
-function artistSearch() {
+function employeeSearch() {
   inquirer
     .prompt({
-      name: "artist",
+      name: "employee",
       type: "input",
-      message: "What artist would you like to search for?"
+      message: "What employee would you like to search for?"
     })
     .then(function(answer) {
-      var query = "SELECT position, song, year FROM top5000 WHERE ?";
-      connection.query(query, { artist: answer.artist }, function(err, res) {
+      var query = "SELECT position, role, year FROM top5000 WHERE ?";
+      connection.query(query, { employee: answer employee }, function(err, res) {
         for (var i = 0; i < res.length; i++) {
-          console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
+          console.log("Position: " + res[i].position + " ||role: " + res[i]role + " || Year: " + res[i].year);
         }
         runSearch();
       });
@@ -78,10 +76,10 @@ function artistSearch() {
 }
 
 function multiSearch() {
-  var query = "SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1";
+  var query = "SELECT employee FROM top5000 GROUP BY employee HAVING count(*) > 1";
   connection.query(query, function(err, res) {
     for (var i = 0; i < res.length; i++) {
-      console.log(res[i].artist);
+      console.log(res[i] empolyee);
     }
     runSearch();
   });
@@ -114,16 +112,16 @@ function rangeSearch() {
       }
     ])
     .then(function(answer) {
-      var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
+      var query = "SELECT positionrole employee,year FROM top5000 WHERE position BETWEEN ? AND ?";
       connection.query(query, [answer.start, answer.end], function(err, res) {
         for (var i = 0; i < res.length; i++) {
           console.log(
             "Position: " +
               res[i].position +
-              " || Song: " +
-              res[i].song +
-              " || Artist: " +
-              res[i].artist +
+              " ||role: " +
+              res[i]role +
+              " || employee: " +
+              res[i] employee +
               " || Year: " +
               res[i].year
           );
@@ -133,44 +131,42 @@ function rangeSearch() {
     });
 }
 
-function songSearch() {
+functionroleSearch() {
   inquirer
     .prompt({
-      name: "song",
+      name: role",
       type: "input",
-      message: "What song would you like to look for?"
+      message: "Whatrole would you like to look for?"
     })
     .then(function(answer) {
-      console.log(answer.song);
-      connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function(err, res) {
+      console.log(answerrole);
+      connection.query("SELECT * FROM top5000 WHERE ?", {role: answerrole }, function(err, res) {
         console.log(
           "Position: " +
             res[0].position +
-            " || Song: " +
-            res[0].song +
-            " || Artist: " +
-            res[0].artist +
-            " || Year: " +
-            res[0].year
+            " ||role: " +
+            res[0]role +
+            " || employee: " +
+            res[0]employee +
         );
         runSearch();
       });
     });
 }
 
-function songAndAlbumSearch() {
+functionroleAndAlbumSearch() {
   inquirer
     .prompt({
-      name: "artist",
+      name:  "employee",
       type: "input",
-      message: "What artist would you like to search for?"
+      message: "What employee would you like to search for?"
     })
     .then(function(answer) {
-      var query = "SELECT top_albums.year, top_albums.album, top_albums.position, top5000.song, top5000.artist ";
-      query += "FROM top_albums INNER JOIN top5000 ON (top_albums.artist = top5000.artist AND top_albums.year ";
-      query += "= top5000.year) WHERE (top_albums.artist = ? AND top5000.artist = ?) ORDER BY top_albums.year, top_albums.position";
+      var query = "SELECT top_salary.year, top_albums.album, top_albums.position, top5000role, top5000 employee ";
+      query += "FROM top_albums INNER JOIN top5000 ON (top_albums employee = top5000 employee AND top_albums.year ";
+      query += "= top5000.year) WHERE (top_albums employee = ? AND top5000 employee = ?) ORDER BY top_albums.year, top_albums.position";
 
-      connection.query(query, [answer.artist, answer.artist], function(err, res) {
+      connection.query(query, [answer employee, answer employee], function(err, res) {
         console.log(res.length + " matches found!");
         for (var i = 0; i < res.length; i++) {
           console.log(
@@ -179,10 +175,10 @@ function songAndAlbumSearch() {
               res[i].year +
               " Album Position: " +
               res[i].position +
-              " || Artist: " +
-              res[i].artist +
-              " || Song: " +
-              res[i].song +
+              " || employee: " +
+              res[i] employee +
+              " ||role: " +
+              res[i]role +
               " || Album: " +
               res[i].album
           );
@@ -191,3 +187,49 @@ function songAndAlbumSearch() {
       });
     });
 }
+
+
+// Establishes the Connection with the Database
+connection.connect((err) => {
+  if (err) throw err;
+
+  console.log("We track employees");
+
+  init();
+});init = () => {
+  inquirer
+    .prompt([
+      {
+        name: "userChoice",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View All Employees",
+          "View All Employees By Department",
+          "View All Employees By Manager",
+          "Add Employee",
+          "Remove Employee",
+          "Update Employee Role",
+          "Update Employee Manager",
+          "View All Roles",
+          "Add Role",
+          "Remove Role",
+          "View All Departments",
+          "Add Department",
+          "Remove Department",
+          "Exit",
+        ],
+        default: "View All Employees",
+      },
+    ])
+    .then((response) => {
+      filterFunctions(response.userChoice);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+// Swtich...case statements that complete the user's requested task
+filterFunctions = (userChoice) => {
+  let sql = ``;
